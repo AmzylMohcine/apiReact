5b7474d24a54a5c9e2dcbaaba153efef0bcfe698
AmzylMohcine <amzylmohcine@gmail.com>
2022-05-03 22:56:56 +0200

    Put and post API

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ function App() {
 
     //get api informations 
     const url = "https://jsonplaceholder.typicode.com/comments";
-    fetch(url)
+    fetch(url , { 
+      headers : { 
+        companyId :'442166'
+      }
+    })
     
     .then(response => response.json()).then(json=> 
       { 
@@ -19,20 +23,63 @@ function App() {
     ).catch(e=> { console.log("e" , e)})  
   
   } , [])
+
+  
+  //post and put api implement 
+  const postPutEvent = () => { 
+    const data = { 
+      id:'501',
+      name :'Mohcine' , 
+      email :'mohcine@gmail.com',
+      body:'body'
+    }
+
+const url = data.id ? "https://jsonplaceholder.typicode.com/comments/"+data.id  :  "https://jsonplaceholder.typicode.com/comments" ;
+    fetch(url , {
+      //Method post pour saisir dans l api
+      method: data.id ? 'PUT' : 'POST',
+      //headers pour definir le format de data
+      headers : { 
+        'Content-Type' : 'application/json', 
+        'Access-Control-Allow-Origin': '*'
+      },
+      //lire la data en format string 
+      body : JSON.stringify(data), 
+
+    })
+    
+    .then(response => { 
+
+      console.log("response" , response);
+
+      if(response.state == 200) { 
+
+        alert("Succes") ; 
+
+      }
+
+    })
+
+    .catch(e=> { console.log("e" , e)})  
+      
+
+  }
   return (
     <div className="App">
-    <h1>Welcome </h1>
-     { data.map(item => { 
-     return (
+    <h1>Comments </h1>
+    { data.map(item => { 
+    return (
     <div>
- {item.email}
+{item.email}
     </div> 
+    
 
-       )
-       
+)
+    
 
-     })
-     }
+    })
+    }
+    <button onClick={postPutEvent}> Submit</button>
     </div>
   );
 }
